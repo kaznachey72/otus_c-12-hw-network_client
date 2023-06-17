941de8221247a8a662ba1105feafec5e734ba706
Dmitriy Kaznacheev <d.kazna@mail.ru>
2023-06-17 16:33:25 +0500

    fix connect error

--- main.c
+++ main.c
@@ -24,33 +24,34 @@ char *create_query_str(const char *font, const char *text)
 
 int create_connect() 
 {
-	struct addrinfo hints = {0};
-	hints.ai_family   = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags    = 0;
-	hints.ai_protocol = IPPROTO_TCP;
+    struct addrinfo hints = {0};
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags    = 0;
+    hints.ai_protocol = IPPROTO_TCP;
 
-	struct addrinfo *res;
-	if (getaddrinfo(HOSTNAME, SERVICE, &hints, &res) != 0) {
+    struct addrinfo *res;
+    if (getaddrinfo(HOSTNAME, SERVICE, &hints, &res) != 0) {
         fprintf(stderr, "getaddrinfo error\n");
-		exit(EXIT_FAILURE);
-	}
+        exit(EXIT_FAILURE);
+    }
 
     int fd = -1;
-	for (struct addrinfo *it_addr = res; it_addr != NULL; it_addr = it_addr->ai_next) {
-		fd = socket(it_addr->ai_family, it_addr->ai_socktype, it_addr->ai_protocol);
-		if (fd == -1) {
+    for (struct addrinfo *it_addr = res; it_addr != NULL; it_addr = it_addr->ai_next) {
+        fd = socket(it_addr->ai_family, it_addr->ai_socktype, it_addr->ai_protocol);
+        if (fd == -1) {
             continue;
         }
-		if (connect(fd, it_addr->ai_addr, it_addr->ai_addrlen) != -1) {
-		    break;
-		}
+        if (connect(fd, it_addr->ai_addr, it_addr->ai_addrlen) != -1) {
+            fprintf(stderr, "connect error\n");
+            break;
+        }
         close(fd);
         fd = -1;
     }
 
-	freeaddrinfo(res);
-	return fd;
+    freeaddrinfo(res);
+    return fd;
 }
 
 void close_connect(int fd)
@@ -100,7 +101,7 @@ int receive_data(int fd, char **msg)
     else {
         free(recv_data);
     }
-	return len;
+    return len;
 }
 
 
@@ -109,49 +110,49 @@ int main (int argc, char **argv)
     char *font = NULL;
     char *text = NULL;
     {
-	    if (argc != 3) {
-		    printf("USAGE: %s <font> <text>\n", argv[0]);
-		    exit(EXIT_FAILURE);
-	    }
+        if (argc != 3) {
+            printf("USAGE: %s <font> <text>\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
         font = argv[1];
         text = argv[2];
     }
 
-	int fd = create_connect();
-	if (fd < 0) {
-		fprintf(stderr, "connect error\n");
-		exit(EXIT_FAILURE);
-	}
+    int fd = create_connect();
+    if (fd < 0) {
+        fprintf(stderr, "connect error\n");
+        exit(EXIT_FAILURE);
+    }
 
     {
-	    if (receive_data(fd, NULL) < 1) {
+        if (receive_data(fd, NULL) < 1) {
             close_connect(fd);
             fprintf(stderr, "no invitation\n");
-		    exit(EXIT_FAILURE);
-	    }
+            exit(EXIT_FAILURE);
+        }
     }
 
     {
         char *query = create_query_str(font, text);
         if (send(fd, query, strlen(query), 0) < 0) {
             free(query);
-		    close_connect(fd);
-		    fprintf(stderr, "send error\n");
-		    exit(EXIT_FAILURE);
-	    }
+            close_connect(fd);
+            fprintf(stderr, "send error\n");
+            exit(EXIT_FAILURE);
+        }
         free(query);
     }
 
     {
         char *message = NULL;
-	    if (receive_data(fd, &message) < 1) {
-		    close_connect(fd);
-		    exit(EXIT_FAILURE);
-	    }
-	    puts(message);
+        if (receive_data(fd, &message) < 1) {
+            close_connect(fd);
+            exit(EXIT_FAILURE);
+        }
+        puts(message);
         free(message);
     }
 
     close_connect(fd);
-	return 0;
+    return 0;
 }
